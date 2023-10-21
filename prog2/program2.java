9b5d3ec71eb456c64440e1b7d274d47ba299f081
sahureshma <ssshureshma17299@gmail.com>
2023-10-21 14:36:10 +0530

    convert roman to integer

---
+++ prog2/program2.java
@@ -0,0 +1,44 @@
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String romanNumeral = "IX";
+
+        int result = 0;
+        int previousValue = 0;
+
+        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+            char currentChar = romanNumeral.charAt(i);
+            int currentValue = romanCharToInt(currentChar);
+
+            if (currentValue < previousValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            previousValue = currentValue;
+        }
+
+        System.out.println("Roman numeral " + romanNumeral + " is equivalent to " + result);
+    }
+
+    public static int romanCharToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
